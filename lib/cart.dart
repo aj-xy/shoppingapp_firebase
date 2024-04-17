bd32158939f981895b7ca15dbe0f114fd54c05b6
aj-xy <ajayshanthanu@gmail.com>
2024-04-17 13:52:25 +0530

    g

--- lib/cart.dart
+++ lib/cart.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Cart extends StatelessWidget {
@@ -19,20 +20,100 @@ class Cart extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                "Bag",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+              Row(
+                children: [
+                  Text(
+                    "Bag",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                  ),SizedBox(width: 25,),
+                  Icon(Icons.shopping_cart,size: 40,)
+                ],
               ),
               SizedBox(
-                height: 20,
+                height: 40,
               ),
               Container(
-                height: 100,
+                height: 150,
                 child: Row(
                   children: [
-                   
+                    Image.network(
+                        'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/d76b79fd-e11d-458d-8177-fa1746921e21/revolution-7-road-running-shoes-Dfvsch.png'),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Nike Revolution 7",
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Womens Running Shoe",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          "Size 5",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        )
+                      ],
+                    )
                   ],
                 ),
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Quantity",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("1",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Transform.rotate(
+                      angle: 270 * 3.1415 / 180,
+                      child: Icon(
+                        Icons.arrow_back_ios_new,
+                      )),
+                  SizedBox(
+                    width: 220,
+                  ),
+                  Text(
+                    "Price",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                color: Colors.grey,
+                thickness: 2,
+              ),
+              Column(crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text("Subtotal"),SizedBox(width: 100,)
+                    ],
+                  ), 
+                  Row(
+                    children: [
+                      Text("Delivery"),SizedBox(width: 100,)
+                    ],
+                  ), 
+                  Row(
+                    children: [
+                      Text("Total",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),SizedBox(width: 100,)
+                    ],
+                  )],
               )
             ],
           ),
--- lib/product.dart
+++ lib/product.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:lottie/lottie.dart';
 import 'package:shoppingapp_firebase/cart.dart';
 
 class Product extends StatelessWidget {
   List data;
   int index;
   Product({required this.data, required this.index, super.key});
+void showAutoDismissDialog(BuildContext context) {
+  Future.delayed(Duration(seconds: 3), () {
+    if (Navigator.canPop(context)) {
+      Navigator.of(context).pop();
+    }
+  });
+
+  showDialog(
+    context: context,
+    builder: (BuildContext builderContext) {
+      // Your popup content here (e.g., AlertDialog, SnackBar)
+      return AlertDialog(
+        title: Text('Added To Cart'),
+        content: Lottie.network('https://lottie.host/8d76dafa-f505-41c8-b04a-73090fad027f/5rYix65fEu.json',),
+      );
+    },
+  );
+}
 
   @override
   Widget build(BuildContext context) {
@@ -168,9 +187,7 @@ class Product extends StatelessWidget {
                                   side: BorderSide(
                                       style: BorderStyle.solid,
                                       color: Colors.black))),
-                          onPressed: () {
-                            
-                          },
+                          onPressed:  () => showAutoDismissDialog(context),
                           child: Text("Add To Cart")),
                       SizedBox(
                         width: 30,
