85ea0688ea98a28bb6a5dcebcd0808fac57a69fe
aj-xy <ajayshanthanu@gmail.com>
2024-04-18 11:46:30 +0530

    ok

--- lib/cart.dart
+++ lib/cart.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:shoppingapp_firebase/shoes.dart';
 
 class Cart extends StatelessWidget {
   Cart({
@@ -17,106 +20,133 @@ class Cart extends StatelessWidget {
           child: Padding(
         padding: EdgeInsets.all(10),
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Text(
-                    "Bag",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
-                  ),SizedBox(width: 25,),
-                  Icon(Icons.shopping_cart,size: 40,)
-                ],
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                height: 150,
-                child: Row(
-                  children: [
-                    Image.network(
-                        'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/d76b79fd-e11d-458d-8177-fa1746921e21/revolution-7-road-running-shoes-Dfvsch.png'),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Nike Revolution 7",
-                          style: TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          "Womens Running Shoe",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        Text(
-                          "Size 5",
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Row(
+              children: [
+                Text(
+                  "Bag",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
                 ),
-              ),
-              Row(
-                children: [
-                  Text(
-                    "Quantity",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text("1",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Transform.rotate(
-                      angle: 270 * 3.1415 / 180,
-                      child: Icon(
-                        Icons.arrow_back_ios_new,
-                      )),
-                  SizedBox(
-                    width: 220,
-                  ),
-                  Text(
-                    "Price",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Divider(
-                color: Colors.grey,
-                thickness: 2,
-              ),
-              Column(crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
+                SizedBox(
+                  width: 25,
+                ),
+                Icon(
+                  Icons.shopping_cart,
+                  size: 40,
+                )
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 300,
+              child: StreamBuilder(
+                stream:
+                    FirebaseFirestore.instance.collection('cart').snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  }
+
+                  return Column(
                     children: [
-                      Text("Subtotal"),SizedBox(width: 100,)
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            return shoes(
+                              data: snapshot.data!.docs,
+                              index: index,
+                            );
+                          },
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "Quantity",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text("1",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18)),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Transform.rotate(
+                              angle: 270 * 3.1415 / 180,
+                              child: Icon(
+                                Icons.arrow_back_ios_new,
+                              )),
+                          SizedBox(
+                            width: 140,
+                          ),
+                          Text(
+                            "Price : ${snapshot.data!.docs[0]['price']}",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Divider(
+                        color: Colors.grey,
+                        thickness: 2,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Text("Subtotal : "),
+                              SizedBox(
+                                width: 274,
+                              ),
+                              Text("${snapshot.data!.docs[0]['price']}")
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text("Delivery"),
+                              SizedBox(
+                                width: 310,
+                              ),
+                              Text("Free")
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "Total",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                              SizedBox(
+                                width: 294,
+                              ),
+                              Text("${snapshot.data!.docs[0]['price']}",style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),)
+                            ],
+                          )
+                        ],
+                      )
                     ],
-                  ), 
-                  Row(
-                    children: [
-                      Text("Delivery"),SizedBox(width: 100,)
-                    ],
-                  ), 
-                  Row(
-                    children: [
-                      Text("Total",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),SizedBox(width: 100,)
-                    ],
-                  )],
-              )
-            ],
-          ),
+                  );
+                },
+              ),
+            ),
+          ]),
         ),
       )),
     );
--- lib/product.dart
+++ lib/product.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:lottie/lottie.dart';
@@ -7,24 +9,44 @@ class Product extends StatelessWidget {
   List data;
   int index;
   Product({required this.data, required this.index, super.key});
-void showAutoDismissDialog(BuildContext context) {
-  Future.delayed(Duration(seconds: 3), () {
-    if (Navigator.canPop(context)) {
-      Navigator.of(context).pop();
-    }
-  });
+  void showAutoDismissDialog(BuildContext context) {
+    Future.delayed(Duration(seconds: 2), () {
+      if (Navigator.canPop(context)) {
+        Navigator.of(context).pop();
+      }
+    });
 
-  showDialog(
-    context: context,
-    builder: (BuildContext builderContext) {
-      // Your popup content here (e.g., AlertDialog, SnackBar)
-      return AlertDialog(
-        title: Text('Added To Cart'),
-        content: Lottie.network('https://lottie.host/8d76dafa-f505-41c8-b04a-73090fad027f/5rYix65fEu.json',),
-      );
-    },
-  );
-}
+    showDialog(
+      context: context,
+      builder: (BuildContext builderContext) {
+        // Your popup content here (e.g., AlertDialog, SnackBar)
+        return AlertDialog(
+          title: Text('Added To Cart'),
+          content: Lottie.network(
+            'https://lottie.host/8d76dafa-f505-41c8-b04a-73090fad027f/5rYix65fEu.json',
+          ),
+        );
+      },
+    );
+  }
+
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Future<void> addData() async {
+    try {
+      CollectionReference cart = firestore.collection('cart');
+      await cart.add({
+        'category': data[index]['category'],
+        'image': data[index]['image'],
+        'model': data[index]['model'],
+        'offer': data[index]['offer'],
+        'price': data[index]['price']
+      });
+      print('Data added successfully');
+    } catch (e) {
+      print('Error adding data :   $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -187,7 +209,10 @@ void showAutoDismissDialog(BuildContext context) {
                                   side: BorderSide(
                                       style: BorderStyle.solid,
                                       color: Colors.black))),
-                          onPressed:  () => showAutoDismissDialog(context),
+                          onPressed: () {
+                            showAutoDismissDialog(context);
+                            addData();
+                          }, 
                           child: Text("Add To Cart")),
                       SizedBox(
                         width: 30,
